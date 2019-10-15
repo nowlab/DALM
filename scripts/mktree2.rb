f9f47c4adc40fa8eee24f27376fc90afaa845da1
MatsuTaku <tkm.matsumoto@gmail.com>
2019-10-16 00:13:51 +0900

    Fix mktree process.

--- scripts/mktree2.rb
+++ scripts/mktree2.rb
@@ -1,34 +1,44 @@
+#!/usr/bin/env ruby
 # coding: utf-8
 
-require_relative 'common'
-
-STDIN.set_encoding("ASCII-8BIT")
-
-ngram_prev=[]
-while gets
-	ngram, value = $_.chomp.split("\t")
-
-	ngram=ngram.split
-	min_size = ngram.size < ngram_prev.size ? ngram.size : ngram_prev.size
-	branch = min_size
-	for i in 1..min_size do
-	    index = min_size-i
-	    if ngram[index] != ngram_prev[index]
-	        branch = index
-	    else
-	        break
-	    end
-	end
-    common_words = ngram[0...branch]
-    for i in branch...ngram.size-1 do
-        puts_inserted(common_words + ngram[branch..i])
-    end
-	if ngram.size > 1 and ngram[-2]=="<#>"
-		puts "#{ngram.join(" ")}\t"
-		puts "#{ngram[-1]} #{ngram[0...-1].join(" ")} \x01<#>\t#{value}"
+def puts_inserted(inserted)
+	if inserted[-1]=="<#>"
+        throw "Irregular ARPA file. Please check your cutoff parameter. B( #{inserted[0...-1].join(" ")} ) is not found, although ARPA has P( * | #{inserted[0...-1].join(" ")} )."
 	else
-		puts "#{ngram.join(" ")}\t#{value}"
+		puts "#{inserted.join(" ")}\t"
 	end
-	ngram_prev = ngram
 end
+
+if __FILE__ == $0
+    STDIN.set_encoding("ASCII-8BIT")
+
+    ngram_prev=[]
+    while gets
+        ngram, value = $_.chomp.split("\t")
+
+        ngram=ngram.split
+        min_size = ngram.size < ngram_prev.size ? ngram.size : ngram_prev.size
+        branch = min_size
+        for i in 1..min_size do
+            index = min_size-i
+            if ngram[index] != ngram_prev[index]
+                branch = index
+            else
+                break
+            end
+        end
+        common_words = ngram[0...branch]
+        for i in branch...ngram.size-1 do
+            puts_inserted(common_words + ngram[branch..i])
+        end
+        if ngram.size > 1 and ngram[-2]=="<#>"
+            puts "#{ngram.join(" ")}\t"
+            puts "#{ngram[-1]} #{ngram[0...-1].join(" ")} \x01<#>\t#{value}"
+        else
+            puts "#{ngram.join(" ")}\t#{value}"
+        end
+        ngram_prev = ngram
+    end
+end
+
 
--- scripts/mktreefile.sh
+++ scripts/mktreefile.sh
@@ -18,9 +18,15 @@ SORT="sort -S 40% -T $(dirname ${OUT}) --compress-program=zstd"
 
 echo "Generate all paths on BST."
 ${MKTREE1} <${ARPA} | pv >${OUT}.tmp1
-echo "Extract all paths for each vertexes."
-LC_ALL=C ${SORT} ${OUT}.tmp1 | ${MKTREE2} | pv >${OUT}.tmp2
+echo "Sorting paths."
+LC_ALL=C ${SORT} ${OUT}.tmp1 >${OUT}.tmp2
 rm ${OUT}.tmp1
-echo "Set probability values for each leaves."
-LC_ALL=C ${SORT} ${OUT}.tmp2 | ${MKTREE3}
+echo "Extract all paths for each vertexes."
+${MKTREE2} <${OUT}.tmp2 | pv >${OUT}.tmp3
 rm ${OUT}.tmp2
+echo "Sorting paths."
+LC_ALL=C ${SORT} ${OUT}.tmp3 >${OUT}.tmp4
+rm ${OUT}.tmp3
+echo "Set probability values for each leaves."
+${MKTREE3} <${OUT}.tmp4
+rm ${OUT}.tmp4
