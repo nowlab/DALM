45dc94c39e372cae3942aa1fef1f070394b30e22
MatsuTaku <tkm.matsutaku@gmail.com>
2019-10-21 20:14:29 +0900

    fix build.sh as building without ninja.

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ if [ ! -e bin ]; then
     mkdir bin
 fi
 cd bin
-cmake -DDALM_NEW_XCHECK=OFF -DDALM_EL_SKIP=OFF -G Ninja ..
+cmake -DDALM_NEW_XCHECK=OFF -DDALM_EL_SKIP=OFF ..
 cmake --build .
 cd ..
 
@@ -14,7 +14,7 @@ if [ ! -e nbin ]; then
     mkdir nbin
 fi
 cd nbin
-cmake -DDALM_NEW_XCHECK=ON -DDALM_EL_SKIP=OFF -G Ninja ..
+cmake -DDALM_NEW_XCHECK=ON -DDALM_EL_SKIP=OFF ..
 cmake --build .
 cd ..
 
@@ -22,7 +22,7 @@ if [ ! -e n1bin ]; then
     mkdir n1bin
 fi
 cd n1bin
-cmake -DDALM_NEW_XCHECK=ON -DDALM_EL_SKIP=ON -G Ninja ..
+cmake -DDALM_NEW_XCHECK=ON -DDALM_EL_SKIP=ON ..
 cmake --build .
 cd ..
 
